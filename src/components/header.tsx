f6bc5f1352814b87e1af183b39b8a6bf5eef11fe
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-01-24 08:20:21 +0900

    ヘッダー調整

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
         <div className="drawer z-50 top-0 sticky">
             <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content flex flex-col">
-                <div className="w-full navbar bg-base-300">
+                <div className="w-full navbar bg-base-300 h-20">
                     <div className="flex-none lg:hidden">
                         <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
