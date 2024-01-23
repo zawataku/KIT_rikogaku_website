f9c2b67be14d5ca05775b53840520fc9195b0bda
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-01-24 04:11:24 +0900

    トップ画像関連の追加・編集

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Header = () => {
     return (
-        <div className="drawer z-50">
+        <div className="drawer z-50 top-0 sticky">
             <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content flex flex-col">
                 <div className="w-full navbar bg-base-300">
@@ -11,23 +11,24 @@ export const Header = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                         </label>
                     </div>
-                    <div className="flex-1 px-2 mx-2">金沢工業大学 理工学基礎プロジェクト</div>
-                    <div className="flex-none hidden lg:block">
+
+                    <div className="flex-1 px-2 mx-2 text-2xl hidden lg:block">金沢工業大学 理工学基礎プロジェクト</div>{/*PC表示時のコンテンツ*/}
+                    <div className="flex-1 px-2 mx-2 text-md lg:hidden">金沢工業大学 理工学基礎プロジェクト</div>
+
+                    <div className="flex-none hidden lg:block">{/*PC表示時のコンテンツ*/}
                         <ul className="menu menu-horizontal">
-                            {/*PC表示時のコンテンツ*/}
                             <li><a>Navbar Item 1</a></li>
                             <li><a>Navbar Item 2</a></li>
                         </ul>
                     </div>
                 </div>
-
             </div>
-            <div className="drawer-side">
+            
+            <div className="drawer-side">{/*スマホ表示時のコンテンツ*/}
                 <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80 min-h-full bg-base-200">
-                    {/*スマホ表示時のコンテンツ*/}
-                    <li><a>Sidebar Item 1</a></li>
-                    <li><a>Sidebar Item 2</a></li>
+                <ul className="menu p-4 w-80 min-h-full bg-base-200 pt-24">
+                    <li><a className="pt-5 pb-5">Sidebar Item 1</a></li>
+                    <li><a className="pt-5 pb-5">Sidebar Item 2</a></li>
                 </ul>
             </div>
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,15 @@ import Link from "next/link";
 
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import Top_Img from "@/components/top_img";
 
 export const Home = () => {
   return (
     <div className="h-screen">
       <Header></Header>
-      <div className="h-screen"></div>
+      <Top_Img></Top_Img>
       <Footer></Footer>
-    </div>  
+    </div>
   )
 };
 export default Home;
