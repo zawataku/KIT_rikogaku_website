f5d8ff3a5a6a0475187254952b35650384b35c6c
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-08-03 19:12:12 +0900

    fix:ヘッダーのレイアウト修正

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -13,12 +13,12 @@ export default function Header() {
     };
 
     return (
-        <header className="header hidden w-full absolute z-30 py-1 px-14 h-20 content-center lg:flex flex-wrap justify-between font-bold text-white bg-gradient-to-b from-black from-5%">
-            <p className="text-3xl">金沢工業大学　理工学基礎プロジェクト</p>
-            <div className="hidden lg:flex text-lg">
-                <a className="block mx-5" href="#about" onClick={(e) => handleScroll(e, "about")}>プロジェクト概要</a>
-                <a className="block mx-5" href="#activity" onClick={(e) => handleScroll(e, "activity")}>活動内容</a>
-                <a className="block mx-5" href="#access" onClick={(e) => handleScroll(e, "access")}>アクセス</a>
+        <header className="header block w-full absolute z-30 py-3 px-3 md:px-14 h-24 content-center lg:flex flex-wrap justify-between font-bold text-white bg-gradient-to-b from-black from-5% text-center lg:text-left">
+            <p className="text-[6vw] md:text-3xl">KIT 理工学基礎プロジェクト</p>
+            <div className="hidden lg:flex text-lg content-center flex-wrap">
+                <a className="block mx-5 hover:text-neutral-500 ease-in-out duration-300" href="#about" onClick={(e) => handleScroll(e, "about")}>プロジェクト概要</a>
+                <a className="block mx-5 hover:text-neutral-500 ease-in-out duration-300" href="#activity" onClick={(e) => handleScroll(e, "activity")}>活動内容</a>
+                <a className="block mx-5 hover:text-neutral-500 ease-in-out duration-300" href="#access" onClick={(e) => handleScroll(e, "access")}>アクセス</a>
             </div>
         </header>
     );
